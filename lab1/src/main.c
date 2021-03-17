9b09ec6d97d8f8ec84bd6c94410c295962eebcee
Jakub Gonet <jakub.gonet@swmansion.com>
2021-03-17 19:48:43 +0100

    Use shared library, use BM functions

--- lab1/src/main.c
+++ lab1/src/main.c
@@ -1,21 +1,19 @@
+#include <bm/block_manager.h>
+#include <bm/vector.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-#include "block_manager.h"
-#include "vector.h"
-
 bool try_extracting_file_pair(char **next_token, char **rest);
 
 int main(int argc, char *argv[]) {
   BM_pairs filename_pairs = NULL;
+  BM_blocks blocks = NULL;
 
   for (int i = 1; i < argc; ++i) {
     const char *extracted = argv[i];
-    printf("read: %s\n", extracted);
     if (!strcmp(extracted, "merge_files")) {
-      printf("merge_files\n");
       char *next_token = NULL;
       char **current_s = &argv[++i];
       while (try_extracting_file_pair(&next_token, current_s)) {
@@ -23,23 +21,23 @@ int main(int argc, char *argv[]) {
         BM_add_pair(&filename_pairs, next_token, *current_s);
         current_s = &argv[++i];
       }
-      BM_blocks blocks = BM_merge_pairs(filename_pairs);
+      blocks = BM_merge_pairs(filename_pairs);
       BM_free_pairs(filename_pairs);
 
-      BM_print_blocks(blocks);
-      BM_free_blocks(blocks);
       argv[i] = next_token;
       --i;
     } else if (!strcmp(extracted, "remove_block")) {
       const unsigned int index = strtol(argv[++i], NULL, 10);
-      printf("remove_block\n");
+      BM_delete_block(blocks, index);
     } else if (!strcmp(extracted, "remove_row")) {
       const unsigned int block_index = strtol(argv[++i], NULL, 10);
       const unsigned int row_index = strtol(argv[++i], NULL, 10);
-      printf("remove_row %d %d\n", block_index, row_index);
+      BM_delete_row(blocks[block_index], row_index);
     } else {
       printf("unrecognized: %s\n", extracted);
     }
+
+    BM_free_blocks(blocks);
   }
 
   return 0;

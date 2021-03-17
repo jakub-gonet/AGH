fc2e8de7fb3938b4fdc73ba731f24b1224309f96
Jakub Gonet <jakub.gonet@swmansion.com>
2021-03-18 00:24:42 +0100

    Add measurements

--- lab1/src/test.c
+++ lab1/src/test.c
@@ -5,10 +5,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/times.h>
 
 #ifdef DYNAMIC
 #include <dlfcn.h>
 #endif
+
+#define MEASURE(MSG, STATEMENT)                                          \
+  do {                                                                   \
+    /* TIME START */                                                     \
+    struct tms tms_measure_start;                                        \
+    clock_t real_measure_start = times(&tms_measure_start);              \
+                                                                         \
+    STATEMENT                                                            \
+                                                                         \
+    /* TIME END */                                                       \
+    struct tms tms_measure_end;                                          \
+    clock_t real_measure_end = times(&tms_measure_end);                  \
+    clock_t real_elapsed = real_measure_end - real_measure_start;        \
+    clock_t user_elapsed =                                               \
+        tms_measure_end.tms_utime - tms_measure_start.tms_utime;         \
+    clock_t system_elapsed =                                             \
+        tms_measure_end.tms_stime - tms_measure_start.tms_stime;         \
+    printf(MSG " real, user, system\n");                                 \
+    printf("%ld %ld %ld\n", real_elapsed, user_elapsed, system_elapsed); \
+  } while (0);
 
 #define LIST_BM_FUNCTIONS(TRANSFORM_SIGNATURE)                               \
   TRANSFORM_SIGNATURE(void, BM_add_pair, BM_pairs *const pairs,              \
@@ -41,7 +62,6 @@ LIST_BM_FUNCTIONS(FPTR_DECL);
 #undef DEF_FPTR
 
 bool try_extracting_file_pair(char **next_token, char **rest);
-
 int main(int argc, char *argv[]) {
 #ifdef DYNAMIC
   void *dl_handle = dlopen("libblock_manager.so", RTLD_LAZY);
@@ -59,15 +79,14 @@ int main(int argc, char *argv[]) {
     if (!strcmp(extracted, "merge_files")) {
       char *next_token = NULL;
       char **current_s = &argv[++i];
-      while (try_extracting_file_pair(&next_token, current_s)) {
-        // *move* ptrs to pairs
-        fptr_BM_add_pair(&filename_pairs, next_token, *current_s);
-        current_s = &argv[++i];
-      }
-      blocks = fptr_BM_merge_pairs(filename_pairs);
-      fptr_BM_print_blocks(blocks);
-      fptr_BM_free_pairs(filename_pairs);
-
+      MEASURE(
+          "parse & allocate",
+          while (try_extracting_file_pair(&next_token, current_s)) {
+            // *move* ptrs to pairs
+            fptr_BM_add_pair(&filename_pairs, next_token, *current_s);
+            current_s = &argv[++i];
+          } blocks = fptr_BM_merge_pairs(filename_pairs);
+          fptr_BM_free_pairs(filename_pairs);)
       argv[i] = next_token;
       --i;
     } else if (!strcmp(extracted, "remove_block")) {

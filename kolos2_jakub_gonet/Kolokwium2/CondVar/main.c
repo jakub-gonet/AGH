7445fc9614bcbdad3f4446a9a943f61d5fdce00f
Jakub Gonet <jakub.gonet@swmansion.com>
2021-06-02 08:25:08 +0200

    Reformat

--- kolos2_jakub_gonet/Kolokwium2/CondVar/main.c
+++ kolos2_jakub_gonet/Kolokwium2/CondVar/main.c
@@ -1,6 +1,6 @@
+#include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <pthread.h>
 
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t var = PTHREAD_COND_INITIALIZER;
@@ -8,44 +8,41 @@ pthread_cond_t var = PTHREAD_COND_INITIALIZER;
 int count = 0;
 
 /*
- * Funkcja 'wait_for_counter' powinna czekać, aż zmienna globalna 'count' chroniona
- * przez mutex 'mutex' osiągnie wartość co najmniej 1000, a następnie zwiększyć ją o 10.
+ * Funkcja 'wait_for_counter' powinna czekać, aż zmienna globalna 'count'
+ * chroniona przez mutex 'mutex' osiągnie wartość co najmniej 1000, a następnie
+ * zwiększyć ją o 10.
  */
 
 void* wait_for_counter(void* arg) {
+  printf("Changing value: %d\n", count);
+  count += 10;
+  //...
 
-    //...
-
-
-    printf("Changing value: %d\n", count);
-    count += 10;
-    //...
-
-    pthread_exit(NULL); 
+  pthread_exit(NULL);
 }
 
 int main(void) {
-    for (int k = 0; k < 100; ++ k) {
-        count = 0;
+  for (int k = 0; k < 100; ++k) {
+    count = 0;
 
-        pthread_t tid;
-	/* Stwórz nowy wątek wykonujący funkcje wait_for_counter.
-	*/
-        int reps = 100000;
-        for (int i = 0; i < reps; ++ i) {
-            pthread_mutex_lock(&mutex);
-            ++ count;
-            pthread_cond_signal(&var);
-            pthread_mutex_unlock(&mutex);
-        }
-	/* Poczekaj na zakończenie stworzonego wątku.
-	*/
-        pthread_mutex_lock(&mutex);
-        if (count != reps + 10) {
-            printf("Wrong value: %d\n", count);
-            exit(-1);
-        }
-        pthread_mutex_unlock(&mutex);
+    pthread_t tid;
+    /* Stwórz nowy wątek wykonujący funkcje wait_for_counter.
+     */
+    int reps = 100000;
+    for (int i = 0; i < reps; ++i) {
+      pthread_mutex_lock(&mutex);
+      ++count;
+      pthread_cond_signal(&var);
+      pthread_mutex_unlock(&mutex);
     }
-    puts("OK");
+    /* Poczekaj na zakończenie stworzonego wątku.
+     */
+    pthread_mutex_lock(&mutex);
+    if (count != reps + 10) {
+      printf("Wrong value: %d\n", count);
+      exit(-1);
+    }
+    pthread_mutex_unlock(&mutex);
+  }
+  puts("OK");
 }

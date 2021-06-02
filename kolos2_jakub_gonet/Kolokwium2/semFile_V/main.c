7ffa83eea25eafceef3b3526e44446720480454c
Jakub Gonet <jakub.gonet@swmansion.com>
2021-06-02 08:22:01 +0200

    Semafor

--- kolos2_jakub_gonet/Kolokwium2/semFile_V/main.c
+++ kolos2_jakub_gonet/Kolokwium2/semFile_V/main.c
@@ -15,6 +15,16 @@
 #define FILE_NAME "common.txt"
 #define SEM_NAME "/kol_sem"
 
+void obtain(int semid) {
+  struct sembuf sem_op = {.sem_num = 0, .sem_op = -1, .sem_flg = 0};
+  int res = semop(semid, &sem_op, 1);
+}
+
+void release(int semid) {
+  struct sembuf sem_op = {.sem_num = 0, .sem_op = 1, .sem_flg = 0};
+  int res = semop(semid, &sem_op, 1);
+}
+
 int main(int argc, char** args) {
   if (argc != 4) {
     printf("Not a suitable number of program parameters\n");
@@ -25,6 +35,16 @@ int main(int argc, char** args) {
   Stworz semafor systemu V
   Ustaw jego wartosc na 1
   ***************************************************/
+
+  union semun {
+    int val;
+    struct semid_ds* buf;
+    unsigned short* array;
+    struct seminfo* __buf;
+  } semun;
+  semun.val = 1;
+  int sem_id = semget(ftok(SEM_NAME, 'x'), 1, IPC_CREAT | IPC_EXCL | 0666);
+  semctl(sem_id, 0, SETVAL, semun);
 
   int fd = open(FILE_NAME, O_WRONLY | O_CREAT | O_TRUNC, 0644);
 
@@ -47,10 +67,11 @@ int main(int argc, char** args) {
       sekcja krytyczna zabezpiecz dostep semaforem
       **********************************************/
 
+      obtain(sem_id);
       sprintf(buf, "Wpis rodzica. Petla %d. Spalem %d\n", parentLoopCounter, s);
       write(fd, buf, strlen(buf));
       write(1, buf, strlen(buf));
-
+      release(sem_id);
       /*********************************
       Koniec sekcji krytycznej
       **********************************/
@@ -65,10 +86,11 @@ int main(int argc, char** args) {
       /*****************************************
       sekcja krytyczna zabezpiecz dostep semaforem
       **********************************************/
-
+      obtain(sem_id);
       sprintf(buf, "Wpis dziecka. Petla %d. Spalem %d\n", childLoopCounter, s);
       write(fd, buf, strlen(buf));
       write(1, buf, strlen(buf));
+      release(sem_id);
 
       /*********************************
       Koniec sekcji krytycznej
@@ -80,7 +102,7 @@ int main(int argc, char** args) {
   /*****************************
   posprzataj semafor
   ******************************/
-
+  semctl(sem_id, -1, IPC_RMID);
   close(fd);
   return 0;
 }

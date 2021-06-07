5b9ee746fb410ea09abaaff9e58b85478b01a353
Jakub Gonet <jakub.gonet@swmansion.com>
2021-06-07 20:33:32 +0200

    Add socket connection functions

--- lab10/client.c
+++ lab10/client.c
@@ -1,6 +1,25 @@
+#include <arpa/inet.h>
+#include <assert.h>
+#include <errno.h>
+#include <netinet/in.h>
+#include <signal.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/un.h>
 
 #include "common.h"
+
+// Globals
+int socket_fd;
+
+int to_int(char* str) {
+  errno = 0;
+  int val = (int)strtol(str, NULL, 10);
+  assert(errno == 0);
+  return val;
+}
 
 void print_board(area_t area) {
   printf("+-----------+\n");
@@ -17,6 +36,64 @@ void print_board(area_t area) {
   printf("+-----------+\n");
 }
 
-int main(int argc, char const* argv[]) {
+int connect_web(char* connection_path) {
+  char* tokenized_port = strtok(connection_path, ":");
+  int port = to_int(tokenized_port);
+  struct sockaddr_in addr = {.sin_family = AF_INET,
+                             // network byte ordered port
+                             .sin_port = htons((uint16_t)port)};
+  // convert ip v4 to binary
+  int ret = inet_pton(AF_INET, connection_path, &addr.sin_addr);
+  assert(ret != 0);
+  // create endpoint
+  int sock = socket(AF_INET, SOCK_STREAM, 0);
+  assert(sock > 0);
+  // connect
+  ret = connect(sock, (struct sockaddr*)&addr, sizeof addr);
+  assert(ret != -1);
+  return sock;
+}
+
+int connect_unix(char* connection_path) {
+  struct sockaddr_un addr = {.sun_family = AF_UNIX};
+  strcpy(addr.sun_path, connection_path);
+  // create endpoint
+  int sock = socket(AF_UNIX, SOCK_STREAM, 0);
+  assert(sock > 0);
+  int ret = connect(sock, (struct sockaddr*)&addr, sizeof addr);
+  assert(ret != -1);
+  return sock;
+}
+
+void terminate(int signum) {
+  UNUSED(signum);
+  exit(EXIT_SUCCESS);
+}
+
+void unregister(void) {
+  // TODO
+  printf("Goodbye!\n");
+}
+
+int main(int argc, char* argv[]) {
+  assert(argc == 3 + 1);
+  signal(SIGINT, &terminate);
+  atexit(&unregister);
+
+  char* name = argv[1];
+  char* mode = argv[2];
+  char* connection_path = argv[3];
+
+  if (strcmp(mode, "web") == 0) {
+    socket_fd = connect_web(connection_path);
+  } else if (strcmp(mode, "unix") == 0) {
+    socket_fd = connect_unix(connection_path);
+  } else {
+    printf("Usage: [name] [web|unix] [ipv4:port | socket path]\n");
+    exit(EXIT_FAILURE);
+  }
+
+  // printf("Waiting for oponent...\n");
+  // printf("Connected with %s\n", NAME);
   return 0;
 }
--- lab10/common.h
+++ lab10/common.h
@@ -1,6 +1,8 @@
 #if !defined(COMMON_H)
 #define COMMON_H
 
+#define UNUSED(...) (void)(__VA_ARGS__)
+
 #define GAME_SIZE 3
 enum cell_type { _ = 0, X = -1, O = 1 };
 typedef enum cell_type area_t[GAME_SIZE][GAME_SIZE];

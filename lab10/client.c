084b8a487884806787683512cfe348c61e253e22
Jakub Gonet <jakub.gonet@swmansion.com>
2021-06-07 21:50:55 +0200

    Add message handling in client

--- lab10/client.c
+++ lab10/client.c
@@ -6,10 +6,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/epoll.h>
 #include <sys/socket.h>
 #include <sys/un.h>
+#include <unistd.h>
 
 #include "common.h"
+
+#define EVENTS_N 2
 
 // Globals
 int socket_fd;
@@ -21,12 +25,15 @@ int to_int(char* str) {
   return val;
 }
 
+char* cell_type_to_str(enum cell_type c) {
+  return c == _ ? " " : c == X ? "X" : c == O ? "O" : NULL;
+}
+
 void print_board(area_t area) {
   printf("+-----------+\n");
   for (size_t y = 0; y < GAME_SIZE; y++) {
     for (size_t x = 0; x < GAME_SIZE; x++) {
-      enum cell_type c = area[y][x];
-      printf("| %s ", c == _ ? " " : c == X ? "X" : c == O ? "O" : "$");
+      printf("| %s ", cell_type_to_str(area[y][x]));
     }
     printf("|\n");
     if (y != GAME_SIZE - 1) {
@@ -65,13 +72,89 @@ int connect_unix(char* connection_path) {
   return sock;
 }
 
+void send_msg(struct message* msg) {
+  write(socket_fd, msg, sizeof(*msg));
+}
+
+void read_msg(struct message* msg) {
+  read(socket_fd, msg, sizeof(*msg));
+}
+
+void send_move(int n) {
+  struct message msg = {.type = msg_move, .payload.move = n};
+  send_msg(&msg);
+}
+
+void send_register(char* name) {
+  struct message msg = {.type = msg_register};
+  strcpy(msg.payload.registered_name, name);
+  send_msg(&msg);
+}
+
+void send_ping(void) {
+  struct message msg = {.type = msg_ping};
+  send_msg(&msg);
+}
+
+void send_disconnect(void) {}
+
+void handle_msg(struct message* msg, area_t* area) {
+  switch (msg->type) {
+    case msg_game_start:
+      printf("Connected with %s\nYou play with %s\n",
+             msg->payload.game_start.oponent_nickname,
+             cell_type_to_str(msg->payload.game_start.character));
+      print_board(*area);
+      break;
+    case msg_new_game_state:
+      memcpy(area, &msg->payload.new_area, sizeof(msg->payload.new_area));
+      print_board(*area);
+      break;
+    case msg_game_end:
+      if (msg->payload.game_end_state == WIN) {
+        printf("You won!\n");
+      } else if (msg->payload.game_end_state == LOSE) {
+        printf("You lost!\n");
+      } else if (msg->payload.game_end_state == TIE) {
+        printf("You tied!\n");
+      }
+      exit(EXIT_SUCCESS);
+      break;
+    case msg_ping:
+      send_ping();
+      break;
+    case msg_username_taken:
+    /* fallthrough */
+    case msg_server_full:
+      /* fallthrough */
+    default:
+      printf(
+          "Server full, username already exists or server issued unknown "
+          "command. Exiting\n");
+      exit(EXIT_FAILURE);
+  }
+}
+
+int epoll_init(void) {
+  int epoll_fd = epoll_create(1);
+
+  struct epoll_event event = {.events = EPOLLIN | EPOLLPRI,
+                              .data = {.fd = STDIN_FILENO}};
+  epoll_ctl(epoll_fd, EPOLL_CTL_ADD, STDIN_FILENO, &event);
+  event.data.fd = socket_fd;
+  epoll_ctl(epoll_fd, EPOLL_CTL_ADD, socket_fd, &event);
+  return epoll_fd;
+}
+
 void terminate(int signum) {
   UNUSED(signum);
+  // noop, make sure that atexit is called
   exit(EXIT_SUCCESS);
 }
 
 void unregister(void) {
-  // TODO
+  send_disconnect();
+  close(socket_fd);
   printf("Goodbye!\n");
 }
 
@@ -93,7 +176,27 @@ int main(int argc, char* argv[]) {
     exit(EXIT_FAILURE);
   }
 
-  // printf("Waiting for oponent...\n");
-  // printf("Connected with %s\n", NAME);
+  send_register(name);
+
+  area_t area = {0};
+
+  int epoll_fd = epoll_init();
+  printf("Waiting for oponent...\n");
+  while (true) {
+    struct epoll_event events[EVENTS_N];
+    ssize_t n_ready = epoll_wait(epoll_fd, events, EVENTS_N, -1);
+    assert(n_ready >= 0);
+    for (size_t i = 0; i < (size_t)n_ready; i++) {
+      if (events[i].data.fd == STDIN_FILENO) {
+        int cell_n = scanf("%d", &cell_n);
+        send_move(cell_n);
+      } else if (events[i].data.fd == socket_fd) {
+        struct message msg;
+        read_msg(&msg);
+        handle_msg(&msg, &area);
+      }
+    }
+  }
+
   return 0;
 }
--- lab10/common.h
+++ lab10/common.h
@@ -1,10 +1,38 @@
 #if !defined(COMMON_H)
 #define COMMON_H
 
+#include <stdbool.h>
+
 #define UNUSED(...) (void)(__VA_ARGS__)
 
 #define GAME_SIZE 3
+#define MAX_NAME_SIZE 16
+enum game_end { TIE, WIN, LOSE };
 enum cell_type { _ = 0, X = -1, O = 1 };
 typedef enum cell_type area_t[GAME_SIZE][GAME_SIZE];
+
+struct message {
+  enum message_type {
+    msg_register,
+    msg_game_start,
+    msg_move,
+    msg_new_game_state,
+    msg_game_end,
+    msg_ping,
+    msg_disconnect,
+    msg_username_taken,
+    msg_server_full
+  } type;
+  union message_payload {
+    struct {
+      char oponent_nickname[MAX_NAME_SIZE];
+      enum cell_type character;
+    } game_start;
+    unsigned move;
+    enum game_end game_end_state;
+    area_t new_area;
+    char* registered_name;
+  } payload;
+};
 
 #endif  // COMMON_H
